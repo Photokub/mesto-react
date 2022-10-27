205c8f17eba11c54eccd83ccc6c68c5b85440006
Photokub <photokub@yandex.ru>
2022-10-27 16:43:05 +0300

    правки

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -43,7 +43,7 @@ export function EditProfilePopup({isOpen, onClose, onUpdateUser}) {
             <label className="form__field">
                 <input className="form__input form__input_type_name" type="text"
                        name="user_name_field"
-                       value={name}
+                       value={name || ''}
                        required minLength="2" maxLength="40"
                        onChange={handleNameChange}
                 />
@@ -51,7 +51,7 @@ export function EditProfilePopup({isOpen, onClose, onUpdateUser}) {
             </label>
             <label className="form__field">
                 <input className="form__input form__input_type_job" type="text" name="user_job_field"
-                       value={description}
+                       value={description || ''}
                        required
                        minLength="2" maxLength="200"
                        onChange={handleJobChange}
